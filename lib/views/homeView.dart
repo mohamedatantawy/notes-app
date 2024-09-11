0a867dfad921be4bab597da00593f0426e197782
Your Name <you@example.com>
2024-09-12 01:48:23 +0300

    create floatingactionbutton

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -6,8 +6,13 @@ class Homeview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      body: const NotesViewBody(),
+      floatingActionButton: FloatingActionButton(
+        shape:const CircleBorder(),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
