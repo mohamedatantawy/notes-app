66e29a14a151f400daaec48fdd91ddde14083019
Your Name <you@example.com>
2024-09-15 01:47:18 +0300

    create new notes status

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const color = Colors.black;
 const kprimarycolor = Color(0xff62FCD7);
 const knotesbox = 'testBox';
+const errormassage = 'there is an error';
--- lib/cubit/add_note_cubit/notes_status.dart
+++ lib/cubit/add_note_cubit/notes_status.dart
@@ -1,5 +1,13 @@
- 
- class NotesStatus {}
- class NotesInitial extends NotesStatus
- {}
- 
+class NotesStatus {}
+
+class NotesInitial extends NotesStatus {}
+
+class AddNotesfailure extends NotesStatus {
+  final String  errormassage;
+
+  AddNotesfailure(this.errormassage);
+}
+
+class AddNotesloading extends NotesStatus {}
+
+class AddNotesSucces extends NotesStatus {}
