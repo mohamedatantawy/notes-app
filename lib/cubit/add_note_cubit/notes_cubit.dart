748a7bf364394eff6f7d2f8a040490382a9bb768
Your Name <you@example.com>
2024-09-15 20:03:53 +0300

    add addnotes cubit

--- lib/cubit/add_note_cubit/notes_cubit.dart
+++ lib/cubit/add_note_cubit/notes_cubit.dart
@@ -1,12 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:myproject/constant.dart';
 import 'package:myproject/cubit/add_note_cubit/notes_status.dart';
 import 'package:myproject/models/notes_Model.dart';
 
-
 class NotesCubit extends Cubit<NotesStatus> {
   NotesCubit() : super(NotesInitial());
-  addnotes(NotesModel note)
-  {
-    
+  addnotes(NotesModel note) async {
+    emit(AddNotesloading());
+    try {
+      var notesbox = Hive.box(knotesbox);
+      emit(AddNotesSucces());
+      await notesbox.add(note);
+    } catch (e) {
+      emit(AddNotesfailure(e.toString()));
+    }
   }
 }
